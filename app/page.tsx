f9d432e0363485a92596d28676f4a01d1d0859b6
Akanksha Singh <akanskahsingh0085@gmail.com>
2025-07-21 11:45:03 +0530

    made cocktails section

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,13 @@ import {ScrollTrigger, SplitText} from 'gsap/all';
 gsap.registerPlugin(ScrollTrigger , SplitText);
 import Navbar from '@/components/Navbar';
 import Hero from '@/components/Hero';
+import Cocktails from '@/components/Cocktails';
 export default function Home() {
   return (
 <>
 <Navbar />
 <Hero/>
-<div className='h-dvh bg-black' ></div>
+<Cocktails/>
 </>
   );
 }
